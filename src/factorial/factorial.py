d1b361375c05832020d719a47970db2ef48483b4
agustin2964 <uruartea@gmail.com>
2026-03-30 20:45:31 -0300

    agrego_factorial

---
+++ src/factorial/factorial.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#*-------------------------------------------------------------------------*
+#* factorial.py                                                            *
+#* calcula el factorial de un número                                       *
+#* Dr.P.E.Colla (c) 2022                                                   *
+#* Creative commons                                                        *
+#*-------------------------------------------------------------------------*
+def factorial(num): 
+    if num < 0: 
+        print("Factorial de un número negativo no existe")
+        return 0
+    elif num == 0: 
+        return 1
+        
+    else: 
+        fact = 1
+        while(num > 1): 
+            fact *= num 
+            num -= 1
+        return fact 
+
+num=int(input())
+print("Factorial ",num,"! es ", factorial(num)) 
