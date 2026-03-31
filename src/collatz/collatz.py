ca2b166ee6a2353203ec7b5016a7800e4450d877
agustin2964 <uruartea@gmail.com>
2026-03-31 11:17:51 -0300

    grafico_collatz

--- src/collatz/collatz.py
+++ src/collatz/collatz.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+valores=[]
 for i in range(1,10001):
     c=0
     num=i
@@ -7,4 +9,11 @@ for i in range(1,10001):
         else:
             num=3*num+1
         c+=1
-    print(f"{i} {c}")
+    valores.append(c)
+print(valores)
+
+plt.plot(range(1, 10001), valores)
+plt.xlabel("Número inicial")
+plt.ylabel("Iteraciones")
+plt.title("Conjetura de Collatz (1 a 10000)")
+plt.show()
